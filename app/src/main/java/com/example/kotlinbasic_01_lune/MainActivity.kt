404ef8f2780a98daf3379f9944e34a9464d63cc1
Lune <lune9402@naver.com>
2021-08-20 20:53:34 +0900

    로그 찍어보기

--- app/src/main/java/com/example/kotlinbasic_01_lune/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic_01_lune/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.kotlinbasic_01_lune
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        clickBtn.setOnClickListener {
+
+//            코드에 메모 남기기-주석
+//            Ctrl+/ : 커서가 있는 줄을 주석 처리.
+//            이 { }안에 있는 코드만 버튼이 눌렸을 때 실행됨.
+
+            Log.d("메인화면로그", "클릭용 버튼이 눌림")
+
+        }
 
 
 
